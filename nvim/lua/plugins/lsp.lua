c56c080a2b143bf9116d039e487fb70bb1de95f6
bibarys1327 <127957260+bibarys1327@users.noreply.github.com>
2025-11-27 19:31:39 +0500

    Update lsp.lua

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,49 +1,56 @@
 return {
   "neovim/nvim-lspconfig",
+
   dependencies = {
     "williamboman/mason.nvim",
     "williamboman/mason-lspconfig.nvim",
-    "hrsh7h/cmp-nvim-lsp",
+    "hrsh7th/cmp-nvim-lsp",
   },
 
   config = function()
     local lspconfig = require("lspconfig")
     local capabilities = require("cmp_nvim_lsp").default_capabilities()
-    
+
     require("mason").setup()
 
+    require("mason-lspconfig").setup({
+      ensure_installed = {
+        "tsserver",
+        "html",
+        "cssls",
+        "jsonls",
+        "eslint",
+        "lua_ls",
+      },
+    })
 
-require("mason-lspconfig").setup({
-  ensure_installed={
-    "ts_ls",
-    "html",
-    "cssls",
-    "jsonls",
-    "eslint",
-    "lua_ls",
-  }
-})
+    local servers = {
+      "tsserver",
+      "html",
+      "cssls",
+      "jsonls",
+      "eslint",
+      "lua_ls",
+    }
 
-local servers = {
-	"ts_ls",
-	"html",
-	"cssls",
-	"jsonls",
-	"eslint",
-	"lua_ls",
+    for _, s in ipairs(servers) do
+      lspconfig[s].setup({
+        capabilities = capabilities,
+      })
+    end
+
+    -- Lua settings
+    lspconfig.lua_ls.setup({
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { "vim" },
+          },
+          workspace = {
+            library = vim.api.nvim_get_runtime_file("", true),
+          },
+        },
+      },
+    })
+  end,
 }
-
-for _, s in ipairs(servers) do
-  lspconfig[s].setup({
-		capabilities = capabilities
-	})
-end
-
-lspconfig.lua_ls.setup({
-	settings = {
-		Lua = {
-			diagnostics = {globals={"vim"}},
-			workspace = {library = vim.api.nvim_get_runtime_file("",true)},
-		}
-	}
-})
