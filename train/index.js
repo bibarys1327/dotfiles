914c68cd604dc32f27b317db6224ef8cc48e959a
bibarys1327 <bibaryssultan1327@gmail.com>
2025-12-14 15:52:22 +0000

    add

--- train/index.js
+++ train/index.js
@@ -1,27 +1,13 @@
-const people = [
-      {
-        name: "Carly",
-        yearOfBirth: 1066,
-      },
-/*      {
-        name: "Ray",
-        yearOfBirth: 1962,
-        yearOfDeath: 2011,
-      },
-      {
-        name: "Jane",
-        yearOfBirth: 1912,
-        yearOfDeath: 1941,
-      },
-*/
-];
-
-const findLast = (people) => {
-  return people.map((p) => {
-    if(p.yearOfBirth === undefined) {
-      p.yearOfBirth = new Date.prototype.getFullYear()
-    };
-  }
-    return p.yearOfBirth
+const totalIntegers = (object) => {
+  let sum = 0;
+  const values = Object.values(object);
+  for (const value of values) {
+    if (typeof value === "number"){
+      sum += 1;
+    }
+  };
+  const nestedObjest = values.filter((value) => typeof value === "object" && value !== null);
+  if (nestedObjest){
+    totalIntegers(nestedObjest)
+  };
 }
-console.log(findLast(people))
