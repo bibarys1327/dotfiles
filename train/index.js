cf4c4aa2235d8c32efe7db3fb3c05005e5c0aaed
bibarys1327 <bibaryssultan1327@gmail.com>
2025-12-15 12:06:46 +0000

    Hello

--- train/index.js
+++ train/index.js
@@ -1,13 +1,24 @@
-const totalIntegers = (object) => {
-  let sum = 0;
-  const values = Object.values(object);
-  for (const value of values) {
-    if (typeof value === "number"){
-      sum += 1;
+function pigLatin(word) {
+  const vowels = ['a','e','i','o','u'];
+  const lower = word.toLowerCase();
+
+  if (vowels.includes(lower[0])) {
+    return word + "way";
+  }
+  let index = 0;
+
+  while (index < lower.length) {
+    if (lower[index] === 'q' && lower[index + 1] === 'u'){
+      index += 2;
+      continue;
     }
-  };
-  const nestedObjest = values.filter((value) => typeof value === "object" && value !== null);
-  if (nestedObjest){
-    totalIntegers(nestedObjest)
-  };
-}
+
+    if(vowels.includes(lower[index])) {
+      break;
+    }
+
+    index++;
+  }
+  return (word.slice(index) + word.slice(0,index) + 'ay');
+};
+
